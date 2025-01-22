166883e85668b9ec4b1c18c1f3bd7888479f7ac5
alamincse97 <alamin.cse97@gmail.com>
2025-01-22 20:54:31 +0600

    [ADD]: access Code

---
+++ starters 170/accessCode.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+/*  freopen(input.txt, 'r', stdin);
+    freopen(output.txt, 'w', stdout); */
+    string s;
+    cin >> s;
+
+    if("WECNITK" == s ){
+        cout << "Welcome to Web Club!" << endl;
+    }                             
+    else
+    cout << "Access denied" << endl;       
+  return 0;
+}
