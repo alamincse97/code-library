4bc901c41c052bee10fdfcdae18d0ab53ed478c9
alamincse97 <alamin.cse97@gmail.com>
2025-03-07 15:55:22 +0600

    [ADD] Perfecto

---
+++ codeforces/Perfecto.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+/*  freopen(input.txt, 'r', stdin);
+    freopen(output.txt, 'w', stdout); */
+    int t;
+    cin >> t;
+    
+    while(t--){
+      long long n;
+      cin >> n;
+      long long num = (n * (n+1)) / 2;
+      long long sq = sqrt(num);
+
+      if(sq * sq == num )
+      {
+        cout << -1 << endl;
+        continue;
+      }
+      long long sum = 0;
+      vector<long long> v;
+
+      for(int i=1; i<=n; i++)
+      {
+        if(!v.empty())
+        {
+          cout << v.back() << " ";
+          sum += v.back();
+          v.pop_back();
+        }
+        else 
+        {
+          long long take = sum+i;
+          long long sq1 = sqrt(take);
+
+          if(sq1 * sq1 == take)
+          {
+            cout << i + 1 << " ";
+            sum += (i + 1);
+            v.push_back(i);
+          }
+          else
+          {
+            cout << i << " ";
+            sum += (i);
+          }
+        }
+      }
+      cout << endl;
+    }
+  return 0;
+}
