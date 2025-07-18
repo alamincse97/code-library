8792878a1b70fc9523d8d659507a5f38dd7bb2a0
alamincse97 <alamin.cse97@gmail.com>
2025-07-19 01:40:42 +0600

    [ADD] h solve

---
+++ bootcamp_contest_01/h.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+/*  freopen(input.txt, 'r', stdin);
+    freopen(output.txt, 'w', stdout); */
+    int a, b;
+    cin >> a >> b;
+    
+    cout <<(int)pow(a, b) << endl;
+
+  return 0;
+}
