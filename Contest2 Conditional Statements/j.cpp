c6e52f11146922c4d662a81b6ff00abe43235715
alamincse97 <alamin.cse97@gmail.com>
2025-07-22 12:57:50 +0600

    [ADD] j solve

---
+++ Contest2 Conditional Statements/j.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+/*  freopen(input.txt, 'r', stdin);
+    freopen(output.txt, 'w', stdout); */
+    int n;
+    cin >> n;
+    
+    int a = n / 3;
+
+    cout << a << endl;
+
+  return 0;
+}
