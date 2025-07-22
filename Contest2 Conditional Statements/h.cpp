4341fa718a85837d31030f93d2e99af20da506f1
alamincse97 <alamin.cse97@gmail.com>
2025-07-22 12:25:51 +0600

    [ADD] h solve

---
+++ Contest2 Conditional Statements/h.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+/*  freopen(input.txt, 'r', stdin);
+    freopen(output.txt, 'w', stdout); */
+    int a, b;
+    cin >> a >> b;
+    
+    int product = a * b;
+
+    if(product % 2 == 0) {
+        cout << "Even" << endl;
+    } else {
+        cout << "Odd" << endl;
+    }
+
+  return 0;
+}
