5e37aef8145dd81291fa4195b8f1a34e4f1c4f15
alamincse97 <alamin.cse97@gmail.com>
2025-07-27 20:11:34 +0600

    [ADD] solve done

---
+++ class_10/rectangle.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+/*  freopen(input.txt, 'r', stdin);
+    freopen(output.txt, 'w', stdout); */
+    int a, b;
+    cin >> a >> b;
+    
+    int area = a * b;
+    int perimeter = 2 * (a + b);
+
+    cout << area << " " << perimeter << endl;
+
+  return 0;
+}
