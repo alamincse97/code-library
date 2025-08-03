836141c59c786042d3c9bf1025b2bd24330e8990
alamincse97 <alamin.cse97@gmail.com>
2025-08-03 17:08:56 +0600

    [ADD] bit problem solve

---
+++ C++/Class_13_bitwise_operator/a.cpp
@@ -0,0 +1,18 @@
+// Problem Link: https://www.codechef.com/practice/course/bit-manipulation/BITMF/problems/BITMEDU1
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+/*  freopen(input.txt, 'r', stdin);
+    freopen(output.txt, 'w', stdout); */
+    
+    cout << (((3&5)|4)^7) << endl;
+
+
+  return 0;
+}
