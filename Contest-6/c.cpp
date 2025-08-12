a49848f369bc66baaad9b4c4bbe60a80ef76106c
alamincse97 <alamin.cse97@gmail.com>
2025-08-12 09:22:03 +0600

    [ADD] c solve

---
+++ Contest-6/c.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+/*  freopen(input.txt, 'r', stdin);
+    freopen(output.txt, 'w', stdout); */
+    
+    int n, k;
+    cin >> n >> k;
+    vector<int> a(n);
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    int ans = 0;
+
+    for(int i=0; i<n; i++){
+        if(a[i] >= a[k-1] && a[i] > 0)ans++;
+    }
+
+    cout << ans << endl;
+
+  return 0;
+}
