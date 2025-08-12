9fddce9b85b895d17146810bb66488a16d7e795f
alamincse97 <alamin.cse97@gmail.com>
2025-08-12 17:31:35 +0600

    [ADD] h solve

---
+++ Contest-6/h.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cin >> n;
+    vector<int> v(n);
+
+    for(int i=0; i<n; i++)cin >> v[i];
+
+    int s=0, d=0, f1=1;
+
+    while (!v.empty() ){
+        if(f1==1){
+            if(v[0] > v.back()){
+                s = s + v[0];
+                v.erase(v.begin() );
+            }
+            else{
+                s = s + v.back();
+                v.pop_back();
+            }
+            f1 = 2;
+        }
+        else{
+            if(v[0] > v.back()){
+                d = d + v[0];
+                v.erase(v.begin());
+            }
+            else{
+                d = d + v.back();
+                v.pop_back();
+            }
+            f1 = 1;
+        }
+    }
+    cout << s << " " << d << endl;
+    return 0;
+}
