e2d70d6d2b00658c2e2b9ce0ab5fe398b3d7d98a
alamincse97 <alamin.cse97@gmail.com>
2025-08-17 20:41:43 +0600

    [ADD] a and b solve

---
+++ C++/class_15_if_else/fan_poll.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+/*  freopen(input.txt, 'r', stdin);
+    freopen(output.txt, 'w', stdout); */
+                                       
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    if(a > b || a > c){
+        cout << "Yes" << endl;
+    }
+    else{
+        cout << "No" << endl;
+    }
+
+  return 0;
+}
