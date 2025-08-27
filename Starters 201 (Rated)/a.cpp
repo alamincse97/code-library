71b434820dd5655d1a4808d7a81ce467119f67bc
alamincse97 <alamin.cse97@gmail.com>
2025-08-27 20:50:40 +0600

    [ADD] a solve codechef

---
+++ Starters 201 (Rated)/a.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+/*  freopen(input.txt, 'r', stdin);
+    freopen(output.txt, 'w', stdout); */
+    int a, b;
+    cin >> a >> b;
+    
+    cout << a + (b * 2) << endl;
+
+
+  return 0;
+}
