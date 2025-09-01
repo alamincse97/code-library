594c855a50bac56f82e1f1958837a43a53f2bc5a
alamincse97 <alamin.cse97@gmail.com>
2025-09-01 12:38:00 +0600

    [ADD] b solve

---
+++ C++/loop/b.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+/*  freopen(input.txt, 'r', stdin);
+    freopen(output.txt, 'w', stdout); */
+
+    int n;
+    scanf("%d", &n);
+
+    int i = 1;
+
+    while(n != 0){
+
+        printf("Case %d: %d\n", i, n);
+        
+        scanf("%d", &n);
+
+        i++;
+
+    }                                    
+
+
+  return 0;
+}
