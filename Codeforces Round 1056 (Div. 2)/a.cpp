6d7830ddb33d5cf722d0538ea3dd3756e0fc2284
alamincse97 <alamin.cse97@gmail.com>
2025-10-05 23:28:28 +0600

    [ADD] a solve

---
+++ Codeforces Round 1056 (Div. 2)/a.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+/*  freopen(input.txt, 'r', stdin);
+    freopen(output.txt, 'w', stdout); */
+    
+    int t;
+    cin >> t;
+    
+    while(t--)
+    {
+        int n;
+        cin >> n;
+
+        cout << 2 * n - 2 << endl;
+    }
+
+  return 0;
+}
