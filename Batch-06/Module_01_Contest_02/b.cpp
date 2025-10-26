d4a31890f058c29f2a640424a0bee8e36b6228d9
alamincse97 <alamin.cse97@gmail.com>
2025-10-26 17:14:19 +0600

    [ADD] d solve

---
+++ Batch-06/Module_01_Contest_02/b.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+/*  freopen(input.txt, 'r', stdin);
+    freopen(output.txt, 'w', stdout); */
+    int a, b;
+    cin >> a >> b;
+    
+    if(a < b)
+    {
+        cout << "a < b" << endl;
+    }
+    else if( a > b)
+    {
+        cout << "a > b" << endl;
+    }
+    else
+    {
+        cout << "a == b" << endl;
+    }
+
+  return 0;
+}
---
+++ Batch-06/Module_01_Contest_02/d.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+/*  freopen(input.txt, 'r', stdin);
+    freopen(output.txt, 'w', stdout); */
+    int x;
+    cin >> x;
+    
+    if(x == 404)
+    {
+        cout << "NOT FOUND" << endl;
+    }
+    else 
+    {
+        cout << "FOUND" << endl;
+    }
+
+  return 0;
+}
