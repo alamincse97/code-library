877c4bc2fa381e5d0c866c11e80508bf671baa2d
alamincse97 <alamin.cse97@gmail.com>
2025-10-26 16:53:44 +0600

    [ADD] a solve

---
+++ Batch-06/Module_01_Contest_02/a.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+/*  freopen(input.txt, 'r', stdin);
+    freopen(output.txt, 'w', stdout); */
+    int n;
+    cin >> n;
+    
+    if(n == 7)
+    {
+        cout << "THALA" << endl;
+    }
+    else{
+        cout << "SADGE" << endl;
+    }
+
+  return 0;
+}
