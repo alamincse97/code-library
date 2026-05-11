37e65a0e40ee4bd137d7c5be0a3571765c56417a
alamincse97 <alamin.cse97@gmail.com>
2026-05-11 13:10:14 +0600

    [ADD] some bug fix

--- Complete CP Course Weekly Contest/CPS Weekly Contest 86 (Junior)/01.cpp
+++ Complete CP Course Weekly Contest/CPS Weekly Contest 86 (Junior)/01.cpp
@@ -1,28 +1,30 @@
 // Problem Link: https://www.codechef.com/problems/CPRIVAL?tab=statement
-#include<bits/stdc++.h>
 
+#include <iostream>
 using namespace std;
 
-int main()
-{
+int main() {
+    // Optimize I/O operations
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-/*  freopen(input.txt, 'r', stdin);
-    freopen(output.txt, 'w', stdout); */
     
     int r1, r2;
     int d1, d2;
 
+    // Read initial ratings and the rating changes
     cin >> r1 >> r2;
     cin >> d1 >> d2;
 
-    int dominates = r1 - d1;
-    int everntes = r2 - d2;
+    // Calculate final ratings by adding the changes
+    int final_dominater = r1 + d1;
+    int final_everule = r2 + d2;
 
-    if(dominates > everntes)
-        cout << "Everule\n";
-    else 
-        cout << "Dominater\n";
+    // Compare and print the winner
+    if (final_dominater > final_everule) {
+        cout << "Dominater" << endl;
+    } else {
+        cout << "Everule" << endl;
+    }
 
-  return 0;
+    return 0;
 }
